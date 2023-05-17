54f200431301f8aa5cea4c953aa2ceeeede9c222
Ilya Vdovenko <Vdovenko.Ilia@yandex.ru>
2023-05-17 20:13:28 +0300

    Add error fixes

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/${artifactId}-${version}.jar
--- src/main/java/com/contur/shortener/linkshortener/repository/SpringDataUrlRepository.java
+++ src/main/java/com/contur/shortener/linkshortener/repository/SpringDataUrlRepository.java
@@ -1,13 +1,17 @@
 package com.contur.shortener.linkshortener.repository;
 
 import com.contur.shortener.linkshortener.entity.Url;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Spring Data jpa specialization of the {@link UrlRepository} interface.
  */
 
-public interface SpringDataUrlRepository extends UrlRepository,
-        PagingAndSortingRepository<Url, Long> {
+public interface SpringDataUrlRepository extends PagingAndSortingRepository<Url, Long> {
 
+        List<Url> findAllByOrderByCountDesc();
+
+        List<Url> findAllByOrderByRankAsc(Pageable pageable);
 }
